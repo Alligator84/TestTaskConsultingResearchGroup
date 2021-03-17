d2674b7c5cd342ff8ad46ab3c986c71109b2eb26
Oleg Yudin <alligator84@Olegs-MacBook-Pro.local>
2021-03-17 17:15:01 +0600

    Create project

---
+++ src/test/java/com/github/alligator84/testshop/TestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alligator84.testshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
