587a7ef76d65e14d9de0135d033c20f633fa0641
Oleg Yudin <olekat84@gmail.com>
2021-03-17 18:46:06 +0600

    Fix id generation

--- src/main/java/com/github/alligator84/testshop/app/domain/Order.java
+++ src/main/java/com/github/alligator84/testshop/app/domain/Order.java
@@ -1,9 +1,6 @@
 package com.github.alligator84.testshop.app.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "orders")
@@ -11,6 +8,7 @@ public class Order {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue
     private Integer id;
 
     public Integer getId() {
